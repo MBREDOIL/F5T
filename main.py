ef2b04e819cbbaa365e063e5fdf7cc078200c324
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-06-03 12:00:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -730,13 +730,13 @@ async def process_drm(
                    cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
 
                 try:
-                    cc = f'[🎥]Vid Id : {str(count).zfill(3)}\n**Video Title :** `{name1} [{res}p] .mkv`\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by ➤ **`{CR}`\n'
-                    cc1 = f'[📕]Pdf Id : {str(count).zfill(3)}\n**File Title :** `{name1} .pdf`\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by➤**`{CR}`\n'
+                    cc = f'[🎥]Vid Id : {str(count).zfill(3)}\n**Video Title :** `{name1} [{res}p] .mkv`\n\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by ➤ **`{CR}`\n'
+                    cc1 = f'[📕]Pdf Id : {str(count).zfill(3)}\n**File Title :** `{name1} .pdf`\n\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by ➤ **`{CR}`\n'
                     #cc1 = f'**◾ {str(count).zfill(3)}:** `{name1} .pdf`\n\n<pre><code>**Title :** {b_name}</code></pre>\n\n**Uploaded by ➤ `{CR}`**\n'
-                    cczip = f'[📁]Zip Id : {str(count).zfill(3)}\n**Zip Title :** `{name1} .zip`\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by ➤ **`{CR}`\n' 
-                    ccimg = f'[🖼️]Img Id : {str(count).zfill(3)}\n**Img Title :** `{name1} .jpg`\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by ➤ **`{CR}`\n'
-                    ccm = f'[🎵]Audio Id : {str(count).zfill(3)}\n**Audio Title :** `{name1} .mp3`\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by ➤ **`{CR}`\n'
-                    cchtml = f'[🌐]Html Id : {str(count).zfill(3)}\n**Html Title :** `{name1} .html`\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by ➤ **`{CR}`\n'
+                    cczip = f'[📁]Zip Id : {str(count).zfill(3)}\n**Zip Title :** `{name1} .zip`\n\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by ➤ **`{CR}`\n' 
+                    ccimg = f'[🖼️]Img Id : {str(count).zfill(3)}\n**Img Title :** `{name1} .jpg`\n\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by ➤ **`{CR}`\n'
+                    ccm = f'[🎵]Audio Id : {str(count).zfill(3)}\n**Audio Title :** `{name1} .mp3`\n\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by ➤ **`{CR}`\n'
+                    cchtml = f'[🌐]Html Id : {str(count).zfill(3)}\n**Html Title :** `{name1} .html`\n\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by ➤ **`{CR}`\n'
                   
                     if "drive" in url:
                         try:
