0ed69b8c627152a9f8dfb9d63051aa378d4dafb4
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-07-13 08:10:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -206,7 +206,7 @@ async def process_drm(
 
                 elif "https://cpvod.testbook.com/" in url:
                     url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
-                    url = f"https://scammer-keys.vercel.app/api?url={url}&token={raw_text4}&auth=@scammer_botxz1
+                    url = f"https://scammer-keys.vercel.app/api?url={url}&token={raw_text4}&auth=@scammer_botxz1"
                     #url = f"https://drmapijion-botupdatevip.vercel.app/api?url={url}&token={raw_text4}"
                     #url = 'https://dragoapi.vercel.app/classplus?link=' + url
                     mpd, keys = helper.get_mps_and_keys(url)
@@ -214,7 +214,7 @@ async def process_drm(
                     keys_string = " ".join([f"--key {key}" for key in keys])
 
                 elif "classplusapp.com/drm/" in url:
-                    url = f"https://scammer-keys.vercel.app/api?url={url}&token={raw_text4}&auth=@scammer_botxz1
+                    url = f"https://scammer-keys.vercel.app/api?url={url}&token={raw_text4}&auth=@scammer_botxz1"
                     #url = f"https://drmapijion-botupdatevip.vercel.app/api?url={url}&token={raw_text4}"
                     #url = 'https://dragoapi.vercel.app/classplus?link=' + url
                     mpd, keys = helper.get_mps_and_keys(url)
@@ -1020,7 +1020,7 @@ async def text_handler(bot: Client, m: Message):
 
             elif "https://cpvod.testbook.com/" in url:
                 url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
-                url = f"https://scammer-keys.vercel.app/api?url={url}&token={raw_text4}&auth=@scammer_botxz1
+                url = f"https://scammer-keys.vercel.app/api?url={url}&token={raw_text4}&auth=@scammer_botxz1"
                 #url = f"https://drmapijion-botupdatevip.vercel.app/api?url={url}&token={raw_text4}"
                 #url = 'https://dragoapi.vercel.app/classplus?link=' + url
                 mpd, keys = helper.get_mps_and_keys(url)
@@ -1028,7 +1028,7 @@ async def text_handler(bot: Client, m: Message):
                 keys_string = " ".join([f"--key {key}" for key in keys])
 
             elif "classplusapp.com/drm/" in url:
-                url = f"https://scammer-keys.vercel.app/api?url={url}&token={raw_text4}&auth=@scammer_botxz1
+                url = f"https://scammer-keys.vercel.app/api?url={url}&token={raw_text4}&auth=@scammer_botxz1"
                 #url = f"https://drmapijion-botupdatevip.vercel.app/api?url={url}&token={raw_text4}"
                 #url = 'https://dragoapi.vercel.app/classplus?link=' + url
                 mpd, keys = helper.get_mps_and_keys(url)
