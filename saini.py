af98b97b7aaac0b1109ba2753221d4dd4d2d665e
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-08-06 08:13:08 +0530

    Update saini.py

--- saini.py
+++ saini.py
@@ -13,8 +13,8 @@ import subprocess
 import concurrent.futures
 from math import ceil
 from utils import progress_bar
-from pyrofork import Client, filters
-from pyrofork.types import Message
+from pyrogram import Client, filters
+from pyrogram.types import Message
 from io import BytesIO
 from pathlib import Path  
 from Crypto.Cipher import AES
